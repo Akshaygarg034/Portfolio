64157f76ec6f1396830419283b768a677bd4ea62
Akshay <20bcs59@nith.ac.in>
2024-04-15 23:48:58 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import GlobalStyle from "./globalStyles";
+
+//Components
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import MySkillsPage from "./components/MySkillsPage";
+
 function App() {
-  return <div>
-    App File
-    </div>
-    
+  const location = useLocation();
+  return (
+    <>
+      <GlobalStyle />
+
+          <Routes key={location.pathname} location={location} >
+
+            <Route path="/" element={<Main />} />
+
+            <Route path="/about" element={<AboutPage />} />
+
+            <Route path="/blog" element={<BlogPage />} />
+
+            <Route path="/work" element={<WorkPage />} />
+
+            <Route path="/skills" element={<MySkillsPage />} />
+
+            <Route path="*" element={<Main />} />
+          </Routes>
+    </>
+  );
 }
 
-export default App
-
+export default App;
