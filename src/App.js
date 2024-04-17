e7327b868cbebd3ae6a2d49ac1dc600e07ab8d90
Akshay <20bcs59@nith.ac.in>
2024-04-17 11:49:35 +0530

    minor bug fixes related to router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Routes, Route, useLocation } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
+import { AnimatePresence } from "framer-motion";
 import GlobalStyle from "./globalStyles";
 
 //Components
@@ -14,7 +17,15 @@ function App() {
     <>
       <GlobalStyle />
 
+      <ThemeProvider theme={lightTheme}>
+
+        {/* For framer-motion animation on page change! */}
+        {/* Changed prop from exitBefore to mode */}
+        <AnimatePresence mode='wait'>
+          {/* Changed Switch to Routes */}
+
           <Routes key={location.pathname} location={location} >
+            {/* Changed component to element */}
 
             <Route path="/" element={<Main />} />
 
@@ -25,9 +36,12 @@ function App() {
             <Route path="/work" element={<WorkPage />} />
 
             <Route path="/skills" element={<MySkillsPage />} />
-
+            {/* Below is to catch all the other routes and send the user to main component,
+you can add custom 404 component or message instead of Main component*/}
             <Route path="*" element={<Main />} />
           </Routes>
+        </AnimatePresence>
+      </ThemeProvider>
     </>
   );
 }
