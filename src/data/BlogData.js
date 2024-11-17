28972a208cab1ab3dd1b7055f32d465945a427e0
Akshay Garg <gargakshay034@gmail.com>
2024-11-17 18:33:20 +0530

    removed unnecessary data and images

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,69 +1,11 @@
 export const Blogs = [
     {
         id:1,
-        name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
+        name:"dummy anme",
         tags:["react","gsap","styled-components"],
         date:"13 May, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        imgSrc:"https://www.google.com/",
         link:"https://www.google.com/"
-    },
-    {
-        id:2,
-        name:"How to create React Hamburger Menu using Styled-Components",
-        tags:["react","styled-components"],
-        date:"5 July, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://www.google.com/u"
-    },
-    {
-        id:3,
-        name:"React Loading Screen: Try these 3 cool loading screens for your app",
-        tags:["react","react-lottie","styled-components"],
-        date:"6 July, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://www.google.com/"
-    },
-    {
-        id:4,
-        name:"How to build a Fabulous Todo App using React, Redux and Framer-Motion",
-        tags:["react","redux","framer-motion"],
-        date:"26 May, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://www.google.com/"
-    },
-    {
-        id:5,
-        name:"What is Higher Order Component (HOC) in React JS?",
-        tags:["react","reactJS","web-development"],
-        date:"3 Feb, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://www.google.com/"
-    },
-    {
-        id:6,
-        name:"How to implement Pagination Component in React from scratch",
-        tags:["react","css"],
-        date:"5 Jan, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://codebucks.hashnode.dev/pagination-in-react"
-
-    },
-   
-    {
-        id:7,
-        name:"What is Redux ? Simply Explained!",
-        tags:["react","redux","javascript"],
-        date:"16 May, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
-    },
-    {
-        id:8,
-        name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
-        tags:["react","framer-motion","styled-components"],
-        date:"26 May, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
-    },
+    }
   
 ] 
--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -1,7 +1,7 @@
 export const experiences = [
     {
-        title: "Summer Analyst",
-        company_name: "Goldman Sachs Pvt Ltd",
+        title: "Title",
+        company_name: "Company",
         bottomColor: "#6d92bf",
         icon: "https://cdn.iconscout.com/icon/free/png-256/free-google-1772223-1507807.png",
         date: "May 2023 - July 2023",
@@ -9,30 +9,6 @@ export const experiences = [
             "Engaged in the Goldman Sachs team maintaining client fee calculation applications",
             "Optimized user experience by decreasing UI loading time by 76% through enhanced MongoDB queries",
             "Rectified over 15 bugs within the internal fee calculation application"
-        ],
-    },
-    {
-        title: "Exam Module 2",
-        company_name: "SGSITS Indore",
-        bottomColor: "#f1df87",
-        icon: "https://cdn.iconscout.com/icon/free/png-256/free-google-1772223-1507807.png",
-        date: "Feb 2023 - Apr 2023",
-        points: [
-            "Developed a robust web-based system to automate the examiner allocation process",
-            "Implemented various functionalities to simplify the examiner allocation workflow",
-            "Built a user-friendly interface with React. Leveraged SQL for efficient database management and Nodejs for integration"
-        ],
-    },
-    {
-        title: "Senior Coordinator",
-        company_name: "#include Club SGSITS",
-        bottomColor: "#bc1010",
-        icon: "https://cdn.iconscout.com/icon/free/png-256/free-google-1772223-1507807.png",
-        date: "Oct 2021 - present",
-        points: [
-            "Developing and maintaining web applications for hackindore using Nextjs and other related technologies",
-            "With the assistance of other coordinators, successfully organized events and guided sessions for students",
-            'Speaker at "Tech-tonic" a programming road-map for freshers',
         ],
     }
 ];
